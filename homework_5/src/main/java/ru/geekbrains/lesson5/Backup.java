6b29208d13fa4b77f83fd946d2a8d28401643cc5
Ivan <ab09@inbox.ru>
2023-09-19 23:31:13 +0500

    добавил рекурсию

--- homework_5/src/main/java/ru/geekbrains/lesson5/Backup.java
+++ homework_5/src/main/java/ru/geekbrains/lesson5/Backup.java
@@ -8,9 +8,9 @@ public class Backup {
 //        backup("C:\\Users\\1\\IdeaProjects\\homeworks\\homework_5\\test_backup\\unnamed.jpg",
 //                "C:\\Users\\1\\IdeaProjects\\homeworks\\homework_5\\test_backup\\copyfile.jpg");
 
-        backupDir("C:\\Users\\1\\IdeaProjects\\homeworks\\homework_5\\test_backup\\");
+        backupDir(PATH);
     }
-    private static final String PATH = "C:\\Users\\1\\IdeaProjects\\homeworks\\test_backup\\text.txt";
+    private static final String PATH = "C:\\Users\\1\\IdeaProjects\\homeworks\\homework_5\\test_backup\\";
     private static boolean isExist = false;
     public static void backup(File file, String fileOut) throws IOException {
         try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)){
@@ -26,27 +26,24 @@ public class Backup {
     public static void backupDir(String dir) throws IOException {
         File homedir = new File(dir);
 
-        // создаю папку для копирования файлов
-
-        if (!isExist){
-        new File(dir+"\\backup\\").mkdir();
-        isExist = true;
-        }
-
         // просмотр папки, которую нужно копировать
         File[] files = homedir.listFiles();
         if (files == null)
             return;
 
+        // создаю папку для копирования файлов
+
+
+        new File(PATH+"\\backup\\").mkdir();
+        new File(PATH+"\\backup\\" + homedir.getName() + "\\").mkdir();
+
         // копирование файлов
-        for (int i = 1; i < files.length; i++){
+        for (int i = 0; i < files.length; i++){
             if (files[i].isDirectory()){
-                continue;
-            }
-            backup(files[i], dir + "\\backup\\" + files[i].getName());
+                new File(PATH+"\\backup\\"+ files[i].getName() + "\\").mkdir();
+                backupDir(files[i].getAbsolutePath());
+            } else if (files[i].isFile())
+            backup(files[i], PATH+"\\backup\\" + homedir.getName() + "\\" + files[i].getName());
         }
-
-
-
     }
 }
